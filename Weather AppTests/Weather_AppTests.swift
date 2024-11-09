5d2913b1d581106d3e3716bca0935dbe2852e6e3
Navee Sratthatad <n4ve@live.com>
2024-11-09 20:49:16 +0700

    Initial Commit

---
+++ Weather AppTests/Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppTests.swift
+//  Weather AppTests
+//
+//  Created by N4ve on 9/11/2567 BE.
+//
+
+import Testing
+@testable import Weather_App
+
+struct Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
